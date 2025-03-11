b2f782fc694ff9c3685c2db3b9b3f3dbe0fcf81f
FarhanIslam123 <166048005+FarhanIslam123@users.noreply.github.com>
2025-03-11 22:10:13 +0600

    upload project

---
+++ ecommarce/script.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.fa-bars').addEventListener('click', (e)=>{
+document.querySelector('.side-nav').classList.add('show-side-nav');
+})
+document.querySelector('.fa-times').addEventListener('click', (e)=>{
+document.querySelector('.side-nav').classList.remove('show-side-nav');
+})
